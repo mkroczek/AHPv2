42b06758f8ad643995a3637d44a572989059799c
mkroczek <1marcin100@gmail.com>
2022-02-18 19:16:19 +0100

    Expert view fix

--- src/main/java/miapd/ahp/gui/ObjectDescription.java
+++ src/main/java/miapd/ahp/gui/ObjectDescription.java
@@ -3,6 +3,8 @@ package miapd.ahp.gui;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import miapd.ahp.objects.ComparisonObject;
 
 import java.util.Map;
@@ -17,16 +19,12 @@ public class ObjectDescription extends GridPane {
         description = new TextArea();
         description.setWrapText(true);
         description.setEditable(false);
+        RowConstraints constraints = new RowConstraints();
+        constraints.setVgrow(Priority.ALWAYS);
+        this.getRowConstraints().addAll(new RowConstraints(), constraints);
         title.setText(object.getProperty("name"));
         add(title, 0, 0);
-//        if (object.getProperties() != null) {
-//            for (Map.Entry<String, String> entry : object.getProperties().entrySet()) {
-//                String name = entry.getKey();
-//                String value = entry.getValue();
-//                description.appendText(name+"\n"+value+"\n\n");
-//            }
-//            add(description, 0, 1);
-//        }
+
         for (Map.Entry<String, String> entry : object.getProperties()) {
             String name = entry.getKey();
             String value = entry.getValue();
