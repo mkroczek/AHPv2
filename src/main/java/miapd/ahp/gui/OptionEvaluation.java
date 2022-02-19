6bff84310ef17112f80ad685104b94e29ecde29a
mkroczek <1marcin100@gmail.com>
2022-02-19 22:24:07 +0100

    Options window fixed

--- src/main/java/miapd/ahp/gui/OptionEvaluation.java
+++ src/main/java/miapd/ahp/gui/OptionEvaluation.java
@@ -1,24 +1,32 @@
 package miapd.ahp.gui;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class OptionEvaluation extends GridPane {
+public class OptionEvaluation extends VBox {
     private Label optionName;
     private ToggleGroup group = new ToggleGroup();
     private Map<String, RadioButton> radioButtons = new LinkedHashMap<>();
 
     public OptionEvaluation(String name, ArrayList<String> options){
         super();
+        this.setAlignment(Pos.CENTER);
         optionName = new Label(name);
-        this.add(optionName, 0, 0, options.size(), 1);
+        this.getChildren().add(optionName);
+        GridPane optionsPart = new GridPane();
+        optionsPart.setAlignment(Pos.CENTER);
+        optionsPart.setHgap(10);
         for (int i = 0 ; i < options.size(); i++){
             String optionName = options.get(i);
             RadioButton radioButton = new RadioButton();
@@ -26,9 +34,11 @@ public class OptionEvaluation extends GridPane {
             radioButton.setToggleGroup(group);
             Label buttonTitle = new Label(optionName);
             radioButtons.put(optionName, radioButton);
-            this.add(buttonTitle, i, 1);
-            this.add(radioButton, i, 2);
+            optionsPart.add(buttonTitle, i, 1);
+            optionsPart.add(radioButton, i, 2);
+            GridPane.setHalignment(radioButton, HPos.CENTER);
         }
+        this.getChildren().add(optionsPart);
     }
 
     public void selectToggle(String alternative){
